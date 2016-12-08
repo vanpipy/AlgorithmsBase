bd9fa63da6c1ddfb0ce8771792066639491f1a51
vanpipy <vanpipy@gmail.com>
2016-12-08 17:37:04 +0800

    Try to add merge sort expository.

---
+++ expository/mergeSort.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+ * Merge sort.
+ *
+ * Original array [x0,y0,x1,y1,x2,y2,] split to X - [x0,x1,x2] and y - [y0,y1,y2]
+ * [x0,x1,x2] split to [x0,x2] and [x1] then merge [x0,x2] and [x1]
+ * 
+ * x/2 = y + z
+ *
+ */
+
+function main (array) {
+    function split () {
+        
+    }
+
+    function merge () {
+        
+    }
+}
