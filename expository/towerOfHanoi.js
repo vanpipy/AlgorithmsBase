f0f71c7845c3da22db40df6013e09cfe7beecc46
vanpipy <vanpipy@gmail.com>
2017-09-06 00:26:39 +0800

    Add tower of hannoi problem.

---
+++ expository/towerOfHanoi.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+ * Hannoi
+ *
+ * Tower solved progress look like as below:
+ *
+ *  | | |    | | |    | | |    1 | |
+ *  | 1 | => | | | => | | | => 2 | |
+ *  | 2 |    | | 1    3 | 1    3 | |
+ *  4 3 |    4 3 2    4 | 2    4 | |
+ *
+ * The less one must be set on the top of the greater one.
+ * Greater and less one represent the width or other attribute.
+ */
+
+const COLUMN = 3;
+const COLUMNHEIGHT = 100;
+
+function main (x, y, z) {
+
+}
+
+module.exports = main
